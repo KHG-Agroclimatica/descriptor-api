44f2088d366501cb0460a75ac46dddbe6464ff1b
KHG-Agroclimatica <khg@agroclimatica.com>
2023-01-12 10:18:08 -0600

    fix type field model

--- src/modules/typeField/models/typeField.model.ts
+++ src/modules/typeField/models/typeField.model.ts
@@ -4,11 +4,13 @@ interface ITypeFieldModel {
   _id: String;
   reference: String;
   name: string;
+  isActive: boolean;
 }
 
 const TypeFieldScheme = new Schema<ITypeFieldModel>({
   name: { type: String, required: true },
   reference: { type: String, required: true },
+  isActive: { type: Boolean, required: true, default: true },
 });
 
 const TypeModel = model<ITypeFieldModel>("TypeField", TypeFieldScheme);
